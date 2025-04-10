41db754ebf96422588cb40b300c052e5b9a38afa
Hatt530 <76796025+Hatt530@users.noreply.github.com>
2025-04-10 14:48:01 -0400

    Update script.js

--- script.js
+++ script.js
@@ -53,15 +53,3 @@ function renderWhatsAppContacts() {
 
 renderContacts();
 renderWhatsAppContacts();
-
-document.getElementById('emailForm').addEventListener('submit', function (e) {
-  e.preventDefault();
-
-  const name = e.target.name.value;
-  const email = e.target.email.value;
-  const message = e.target.message.value;
-
-  const mailtoLink = `mailto:nobilistechcompany@gmail.com?subject=Message from ${encodeURIComponent(name)}&body=${encodeURIComponent(message)}%0A%0AFrom: ${encodeURIComponent(email)}`;
-
-  window.location.href = mailtoLink;
-});
