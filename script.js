a747571bf2060d9c6761ddb515e9dca139bd8a4d
Hatt530 <76796025+Hatt530@users.noreply.github.com>
2025-04-17 23:46:35 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function openWhatsApp() {
   const phoneNumber = '18763235647'; // Country code + number
-  const message = encodeURIComponent("Hi! I need a pick up from ");
+  const message = encodeURIComponent("Hi! I’d like to get in touch.");
   const url = `https://wa.me/${phoneNumber}?text=${message}`;
   window.open(url, '_blank');
 }
